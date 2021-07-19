fc1ce6df6dadcf4486b0630d32c9d53589e04e3b
messingsang <64595100+messingsang@users.noreply.github.com>
2021-07-19 11:15:46 +0200

    Create checks.js
    put js checks in separate file

---
+++ checks.js
@@ -0,0 +1,26 @@
+function checks() {
+sslcheck();
+fbcheck();
+}
+
+
+function sslcheck() {
+if (location.protocol === 'https:') {
+// page is secure excellent
+document.getElementById("sslyes1").innerHTML="https check done";
+}
+else {
+//punish the user for not using https
+document.getElementById("wholebody").innerHTML="You miserable fool you forgot to type in https://";
+}
+}
+
+function fbcheck() {
+//punishes the user (somehow) for using fbclid
+if (location.search.includes("fbclid")) {
+document.getElementById("fbannoyance").innerHTML="Oh you came here from fb? Interesting shit! I wonder what I am going to do with that information in the future...";
+}
+else {
+document.getElementById("fbannoyance").style.display="none";
+}
+}
