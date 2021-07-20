a32ba0ee3a69b9d40fb54aa8c26aa33761b19629
messingsang <64595100+messingsang@users.noreply.github.com>
2021-07-20 09:54:34 +0200

    messing around with some content loading

--- loader.js
+++ loader.js
@@ -2,6 +2,12 @@ async function loadContent(url) {
   return await (await fetch(url)).text();
 }
 
-async function loadAbout() {
-  document.getElementById("content").innerHTML = await loadContent("about.html");
+async function loadFromMenu(conStr) {
+  switch (conStr){
+    case 'about':
+      document.getElementById("content").innerHTML = await loadContent("about.html");
+    break;
+    default:
+      document.getElementById("content").innerHTML = "Whoa dude something went wrong!"
+
 }
