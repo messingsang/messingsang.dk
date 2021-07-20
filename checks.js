2aca3b2a16268836dfd38388163accf79400c860
emilonraspi <emil.n.jakobsen@gmail.com>
2021-07-20 21:19:07 +0100

    Made a few layout changes. Enabled 90's mode.

--- checks.js
+++ checks.js
@@ -8,12 +8,12 @@ fbcheck();
 function sslcheck() {
 if (location.protocol === 'https:') {
 // page is secure excellent
-document.getElementById("sslyes1").innerHTML="https check done";
+document.getElementById("sslyes1").innerHTML="90's mode enabled";
 }
-else {
+/*else {
 //punish the user for not using https
 document.getElementById("wholebody").innerHTML="Please wait until https is available on this site thank you";
-}
+}*/
 }
 
 function fbcheck() {
