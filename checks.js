4c22dfff62490286e87c483ba4bcfedda0fd2f8c
messingsang <64595100+messingsang@users.noreply.github.com>
2021-07-20 07:24:39 +0200

    Update checks.js

--- checks.js
+++ checks.js
@@ -11,7 +11,7 @@ document.getElementById("sslyes1").innerHTML="https check done";
 }
 else {
 //punish the user for not using https
-document.getElementById("wholebody").innerHTML="You miserable fool you forgot to type in https://";
+document.getElementById("wholebody").innerHTML="Please wait until https is available on this site thank you";
 }
 }
 
