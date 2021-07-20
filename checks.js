c7dab041b573570384c57eddcdde4d40d0ad53a8
messingsang <64595100+messingsang@users.noreply.github.com>
2021-07-20 14:10:52 +0200

    Update checks.js
    There's the culprit

--- checks.js
+++ checks.js
@@ -3,14 +3,6 @@ sslcheck();
 fbcheck();
 }
 
-async function loadContent(url) {
-  return await (await fetch(url)).text();
-}
-
-async function loadAbout() {
-  document.getElementById("content").innerHTML = await loadContent("about.html");
-}
-
 
 
 function sslcheck() {
