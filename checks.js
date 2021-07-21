e0e65bb3b98492df673b9aa7d8de00fd530531ca
emil on deb <emil.n.jakobsen@gmail.com>
2021-07-21 21:14:25 +0200

    minor changes

--- checks.js
+++ checks.js
@@ -10,7 +10,8 @@ if (location.protocol === 'https:') {
 // page is secure excellent
 document.getElementById("sslyes1").innerHTML="90's mode enabled";
 }
-/*else {
+/*currently disabled for testing. I also don't need it anymore to be fair.
+else {
 //punish the user for not using https
 document.getElementById("wholebody").innerHTML="Please wait until https is available on this site thank you";
 }*/
