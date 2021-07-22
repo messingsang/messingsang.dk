48d5ca1248c7467f40ff0955694d2362b6106cac
emil on deb <emil.n.jakobsen@gmail.com>
2021-07-22 10:42:24 +0200

    fixed the thing I broke when fixing the last thing

--- countdown.js
+++ countdown.js
@@ -1,23 +1,32 @@
-function countdown(count){
-  //init
-  //var count = seconds;
+var count=0;
+
+function trycountdown(time){
+
+
+
+/* don't do it if it's already running*/
+  if ((count==0)){
+//init
+count=time;
+document.getElementById("counter").innerHTML = count;
+document.getElementById("countertext").innerHTML =  "OH FUCK YOU STARTED THE COUNTDOWN";
+document.getElementById("butt").style.display = "none";
+//every second
+var x = setInterval(function() {
+
+
+  //subtract from count
+  count=count-1;
+  //update counter
   document.getElementById("counter").innerHTML = count;
-  document.getElementById("countertext").innerHTML =  "OH FUCK YOU STARTED THE COUNTDOWN";
-  document.getElementById("butt").style.display = "none";
-  //every second
-  var x = setInterval(function() {
-    //subtract from count
-    count=count-1;
-    //update counter
-    document.getElementById("counter").innerHTML = count;
 
-    if (count<=5) {
-      document.getElementById("countertext").innerHTML = "SHIT ITS ALMOST THERE!";
-    }
-    if (count<=0) {
-      clearInterval(x);
-      document.getElementById("countertext").innerHTML = "oh.";
-    }
-
+  if (count<=5) {
+    document.getElementById("countertext").innerHTML = "SHIT ITS ALMOST THERE!";
+  }
+  if (count<=0) {
+    clearInterval(x);
+    document.getElementById("countertext").innerHTML = "oh.";
+  }
 }, 1000);
+}
 }
