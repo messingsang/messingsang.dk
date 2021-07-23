a2abed643a87246018cd696340b10527315cd3eb
messingsang <64595100+messingsang@users.noreply.github.com>
2021-07-23 13:07:23 +0200

    improvements to countdown.js

--- countdown.js
+++ countdown.js
@@ -1,30 +1,35 @@
 var count=0;
 
 function updateTimer(){
-  //subtract from count
-  count--;
   //update display
   document.getElementById("counter").innerHTML = count;
+  //additional fun
+  if (count == 5){
+    document.getElementById("countertext").innerHTML =  "SHIT IT'S ALMOST THERE!";
+  }
+  if (count == 0){
+    document.getElementById("countertext").innerHTML =  "Oh. I guess this isn't implemented yet.";
+  }
+
 }
 
 
 
 function trycountdown(time){
-/* is the countdown initialized?*/
-  if ((count==0)){
-    //init
+//is the input a positive integer?
+  if ((time > 0)&&(Number.isInteger(time)){
+    //good now init
     count=time;
     document.getElementById("counter").innerHTML = count;
     document.getElementById("countertext").innerHTML =  "OH FUCK YOU STARTED THE COUNTDOWN";
     document.getElementById("butt").style.display = "none";
-    
+  
     //okay now do this shit until count reaches 0 or below
     while (count>0){
-    
-      //is count a positive integer?
-      if ((count > 0)&&(Number.isInteger(count)){
-         //update timer with 1000ms delay
-         setTimeout(updateTimer(), 1000);
+      //subtract from count
+      count--;
+      //update timer after 1000ms delay
+      setTimeout(updateTimer(), 1000);
       }
     }
   }
