317d1cd88f058c24900b7b6db2d10964e19f1b70
messingsang <64595100+messingsang@users.noreply.github.com>
2021-07-23 12:47:26 +0200

    countdown.js rewritten

--- countdown.js
+++ countdown.js
@@ -1,32 +1,32 @@
 var count=0;
 
+function updateTimer(){
+  //subtract from count
+  count--;
+  //update display
+  document.getElementById("counter").innerHTML = count;
+}
+
+
+
 function trycountdown(time){
-
-
-
 /* don't do it if it's already running*/
   if ((count==0)){
 //init
-count=time;
-document.getElementById("counter").innerHTML = count;
-document.getElementById("countertext").innerHTML =  "OH FUCK YOU STARTED THE COUNTDOWN";
-document.getElementById("butt").style.display = "none";
-//every second
-var x = setInterval(function() {
-
-
-  //subtract from count
-  count=count-1;
-  //update counter
-  document.getElementById("counter").innerHTML = count;
-
-  if (count<=5) {
-    document.getElementById("countertext").innerHTML = "SHIT ITS ALMOST THERE!";
+    count=time;
+    document.getElementById("counter").innerHTML = count;
+    document.getElementById("countertext").innerHTML =  "OH FUCK YOU STARTED THE COUNTDOWN";
+    document.getElementById("butt").style.display = "none";
+    
+    //is count a positive integer?
+    if ((count > 0)&&(Number.isInteger(count)){
+        //update timer with 1000ms delay
+        setTimeout(updateTimer(), 1000);
   }
-  if (count<=0) {
-    clearInterval(x);
-    document.getElementById("countertext").innerHTML = "oh.";
-  }
-}, 1000);
+  /*
+    else {
+       // no need to clear async
+    }
+  */
 }
-}
+
