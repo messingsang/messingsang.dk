a8978e8394ad4610a6342c378ca658217374c9b5
messingsang <64595100+messingsang@users.noreply.github.com>
2021-07-25 14:53:27 +0200

    fucked with the loader
    I hope this works

--- loader.js
+++ loader.js
@@ -1,16 +1,21 @@
-async function loadContent(url) {
+async function preloadContent(url) {
   return await (await fetch(url)).text();
 }
+async function preloadImage(url){
+  return await (await fetch(url));
+}
+
 
 async function loadFromMenu(str) {
       if (str === 'weed'){
-      document.getElementById("content").innerHTML = await loadContent("content_html/cannabis.html");
+      document.getElementById("everything").style["background-image"] = await preloadImage(url("images/weed.jpg");
+      document.getElementById("content").innerHTML = await preloadContent("content_html/cannabis.html");
       }
       else if (str === 'supersecretshit'){
-      document.getElementById("content").innerHTML = await loadContent("content_html/supersecretpasswordentry.html");
+      document.getElementById("content").innerHTML = await preloadContent("content_html/supersecretpasswordentry.html");
       }
       else if (str==='about'||str==='questions'||str==='classified'||str==='thanks'){
-        document.getElementById("content").innerHTML = await loadContent("content_html/" + str + ".html");
+        document.getElementById("content").innerHTML = await preloadContent("content_html/" + str + ".html");
       }
       else{
         console.log("something weird is going on in the load function");
