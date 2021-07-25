c06e167f58b21c42dc154b00d400511c53825957
messingsang <64595100+messingsang@users.noreply.github.com>
2021-07-25 17:19:56 +0200

    important update to bghandler.js

--- bghandler.js
+++ bghandler.js
@@ -1,11 +1,11 @@
 function bgchange(str){
   if (str=='none'){
-    document.getElementById("bgoverlay").style["background-color"] = "rgba(0,0,0,1)";
-    //document.getElementById("everything").style["background-image"] = "none";
+    //document.getElementById("bgoverlay").style["background-color"] = "rgba(0,0,0,1)";
+    document.getElementById("everything").style["background-image"] = "url(images/dummy_black.jpg)";
   }
   else {
     document.getElementById("everything").style["background-image"] = "url(images/" + str + ".jpg)";
-    document.getElementById("bgoverlay").style["background-color"] = "rgba(0,0,0,0.7)";
+    //document.getElementById("bgoverlay").style["background-color"] = "rgba(0,0,0,0.7)";
   }
 
 }
