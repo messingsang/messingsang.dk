4cdbac7d93d456ec46b1f1d85fa48a807a732f40
messingsang <64595100+messingsang@users.noreply.github.com>
2021-07-25 19:07:07 +0200

    weird errors with that var

--- loader.js
+++ loader.js
@@ -2,34 +2,34 @@ async function preloadContent(url) {
   return await (await fetch(url)).text();
 }
 
-var location='lander';
+var menu_location='lander';
 async function loadFromMenu(str) {
       //first we blackout by default
       //bgchange('none'); on second thought let's not blackout by default
       //it is a silly thing to do
-      if (location=='ressource'&&!str=='ressource'){
+      if (menu_location=='ressource'&&!str=='ressource'){
       overlaychange('none');
       }
       //then firgure out what we wanna do
       if (str == 'weed'){
         bgchange('weed');
         document.getElementById("content").innerHTML = await preloadContent("content_html/cannabis.html");
-        location='weed';
+        menu_location='weed';
       }
       else if (str == 'supersecretshit'){
         bgchange('none');
         document.getElementById("content").innerHTML = await preloadContent("content_html/supersecretpasswordentry.html");
-        location='supersecretshit';
+        menu_location='supersecretshit';
       }
       else if (str=='about'||str==='questions'||str==='classified'||str==='thanks'){
         bgchange(str);
         document.getElementById("content").innerHTML = await preloadContent("content_html/" + str + ".html");
-        location='other';
+        menu_location='other';
       }
       else if (str=='ressource'){
         document.getElementById("content").innerHTML = await preloadContent("content_html/ressource.html");
         overlaychange('reddish');
-        location='ressource';
+        menu_location='ressource';
       }
       else{
         console.log("something weird is going on in the load function");
