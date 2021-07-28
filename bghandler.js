c7d9b986337adaf3e8249b9f0575328ef47d0551
messingsang <64595100+messingsang@users.noreply.github.com>
2021-07-28 09:51:12 +0200

    big improvements to styling

--- bghandler.js
+++ bghandler.js
@@ -1,15 +1,15 @@
 function bgchange(str){
   if (str=='none'){
     //document.getElementById("bgoverlay").style["background-color"] = "rgba(0,0,0,1)";
-    document.getElementById("everything").style["background-image"] = "url(images/dummy_black.jpg)";
+    document.getElementById("bgphoto").style["background-image"] = "url(images/dummy_black.jpg)";
   }
   else if (str=='weed'||str=='weed2'||str=='youwerenew'){
-    document.getElementById("everything").style["background-image"] = "url(images/" + str + ".jpg)";
+    document.getElementById("bgphoto").style["background-image"] = "url(images/" + str + ".jpg)";
     //document.getElementById("bgoverlay").style["background-color"] = "rgba(0,0,0,0.7)";
   }
   else {
     //default to the dummy bg
-    document.getElementById("everything").style["background-image"] = "url(images/dummy_black.jpg)";
+    document.getElementById("bgphoto").style["background-image"] = "url(images/dummy_black.jpg)";
   }
 
 }
--- messingsang-menu.js
+++ messingsang-menu.js
@@ -4,7 +4,7 @@ function toggle_menu_collapse(str) {
 
   if (str==='main'&&menu_on===true){
     //grab array of buttondivs
-    var bdvs=document.getElementsByClassName("buttondivs l1");
+    var bdvs=document.getElementsByClassName("menu_buttondivs_l1");
 
     //change height of all buttondivs to 0
     for(let i=0; i<bdvs.length; i++) {
@@ -14,7 +14,7 @@ function toggle_menu_collapse(str) {
     }
     //if any submenus are on close em
     if (about_submenu_on==true) {
-      toggle_submenu_collapse('about');
+      toggle_submenu_collapse();
     }
 
     //unreserve menucontainer
@@ -24,7 +24,7 @@ function toggle_menu_collapse(str) {
 
   }
   else if (str==='main'&&menu_on===false){
-    var bdvs=document.getElementsByClassName("buttondivs l1");
+    var bdvs=document.getElementsByClassName("menu_buttondivs_l1");
     //reserve some space with the menucontainer immediately
     //document.getElementById("menucontainer").style.height=(74*bdvs.length+"px");
     //change height of all buttondivs back to 70
@@ -38,31 +38,22 @@ function toggle_menu_collapse(str) {
   }
 }
 
-function toggle_submenu_collapse(str){
-    if (str==='about'&&about_submenu_on===true){
-    //grab array of buttondivs
-    var sbdvs=document.getElementsByClassName("buttondivs l2");
-    //change height of all buttondivs to 0
+function toggle_submenu_collapse(){
+    if (about_submenu_on===true){
+    var sbdvs=document.getElementsByClassName("menu_buttondivs_l2");
     for(let i=0; i<sbdvs.length; i++) {
     sbdvs[i].style.pointerEvents = "none";
     sbdvs[i].style.height = "0px";
     sbdvs[i].style.opacity = "0";
-    //sbdvs[i].style.border = "none";
     }
-    //unreserve space
-    //document.getElementById("aboutsubcontainer").style.height=("0px");
     about_submenu_on=false;
   }
-  else if (str==='about'&&about_submenu_on===false){
-    var sbdvs=document.getElementsByClassName("buttondivs l2");
-    //reserve some more space in the menucontiner
-    //document.getElementById("aboutsubcontainer").style.height=(54*sbdvs.length+"px");
-    //change height of all the right divs back to 50
+  else if (about_submenu_on===false){
+    sbdvs=document.getElementsByClassName("menu_buttondivs_l2");
     for(let i=0; i<sbdvs.length; i++) {
     sbdvs[i].style.pointerEvents = "auto";
     sbdvs[i].style.height = "50px";
     sbdvs[i].style.opacity = "1";
-    //sbdvs[i].style.border = "2px solid transparent";
     }
     about_submenu_on=true;
   }
